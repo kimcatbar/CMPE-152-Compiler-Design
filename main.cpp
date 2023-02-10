4dc05539b67380bdab8901e30e157c70ba125b48
Kimberly Manzano <kimberly.manzano@sjsu.com>
2023-02-09 17:27:00 -0800

    it kinda works just doesn't read symbols

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 //Create an enumerated type to represent the token labels
-enum class TokenType {
+/*enum class TokenType {
     AND,
     ARRAY,
     ASM,
@@ -99,99 +99,18 @@ enum class TokenType {
     RBRACE,
     LCOMMENT,
     RCOMMENT,
-    //eof
-};
+    EOF
+};*/
 
 class Scanner {
     public:
     Scanner() {
         /* set up possible input word as key, and tokentype as value */
-        keyword["and"] = TokenType::AND;
-        keyword["array"] = TokenType::ARRAY;
-        keyword["asm"] = TokenType::ASM;
-        keyword["begin"] = TokenType::BEGIN;
-        keyword["break"] = TokenType::BREAK;
-        keyword["case"] = TokenType::CASE;
-        keyword["const"] = TokenType::CONST;
-        keyword["constructor"] = TokenType::CONSTRUCTOR;
-        keyword["continue"] = TokenType::CONTINUE;
-        keyword["destructor"] = TokenType::DESTRUCTOR;
-        keyword["div"] = TokenType::DIV;
-        keyword["do"] = TokenType::DO;
-        keyword["downto"] = TokenType::DOWNTO;
-        keyword["else"] = TokenType::ELSE;
-        keyword["end"] = TokenType::END;
-        keyword["FALSE"] = TokenType::FALSE;
-        keyword["file"] = TokenType::FILE;
-        keyword["for"] = TokenType::FOR;
-        keyword["function"] = TokenType::FUNCTION;
-        keyword["goto"] = TokenType::GOTO;
-        keyword["if"] = TokenType::IF;
-        keyword["implementation"] = TokenType::IMPLEMENTATION;
-        keyword["in"] = TokenType::IN;
-        keyword["inline"] = TokenType::INLINE;
-        keyword["interface"] = TokenType::INTERFACE;
-        keyword["label"] = TokenType::LABEL;
-        keyword["mod"] = TokenType::MOD;
-        keyword["nil"] = TokenType::NIL;
-        keyword["not"]= TokenType::NOT;
-        keyword["object"] = TokenType::OBJECT;
-        keyword["of"] = TokenType::OF;
-        keyword["on"] = TokenType::ON;
-        keyword["operator"] = TokenType::OPERATOR;
-        keyword["or"] = TokenType::OR;
-        keyword["packed"] = TokenType::PACKED;
-        keyword["procedure"] = TokenType::PROCEDURE;
-        keyword["program"] = TokenType::PROGRAM;
-        keyword["record"] = TokenType::RECORD;
-        keyword["repeat"] = TokenType::REPEAT;
-        keyword["set"] = TokenType::SET;
-        keyword["shl"] = TokenType::SHL;
-        keyword["SHR"] = TokenType::SHR;
-        keyword["string"] = TokenType::STRING;
-        keyword["then"] = TokenType::THEN;
-        keyword["to"] = TokenType::TO;
-        keyword["TRUE"] = TokenType::TRUE;
-        keyword["type"] = TokenType::TYPE;
-        keyword["unit"] = TokenType::UNIT;
-        keyword["until"] = TokenType::UNTIL;
-        keyword["uses"] = TokenType::USES;
-        keyword["var"] = TokenType::VAR;
-        keyword["while"] = TokenType::WHILE;
-        keyword["with"] = TokenType::WITH;
-        keyword["xor"] = TokenType::XOR;
-        keyword["(integer)"] = TokenType::INTEGER;
-        keyword["(real number)"] = TokenType::REAL;
-        keyword["(identifier)"] = TokenType::INDENTIFIER;
-        keyword["+"] = TokenType::PLUSOP;
-        keyword["-"] = TokenType::MINUSOP;
-        keyword["*"] = TokenType::MULTOP;
-        keyword["/"] = TokenType::DIVOP;
-        keyword[":="] = TokenType::ASSIGN;
-        keyword["="] = TokenType::EQUAL;
-        keyword["<>"] = TokenType::NE;
-        keyword["<="] = TokenType::LTEQ;
-        keyword[">="] = TokenType::GTEQ;
-        keyword["<"] = TokenType::LT;
-        keyword[">"] = TokenType::GT;
-        keyword["+="] = TokenType::PLUSEQUAL;
-        keyword["-=]"] = TokenType::MINUSEQUAL;
-        keyword["*="] = TokenType::MULTEQUAL;
-        keyword["/="] = TokenType::DIVEQUAL;
-        keyword["^"] = TokenType::CARAT;
-        keyword[";"] = TokenType::SEMICOLON;
-        keyword[","] = TokenType::COMMA;
-        keyword["("] = TokenType::LPAREN;
-        keyword[")"] = TokenType::RPAREN;
-        keyword["["] = TokenType::LBRACKET;
-        keyword["]"] = TokenType::RBRACKET;
-        keyword["{"] = TokenType::LBRACE;
-        keyword["}"] = TokenType::RBRACE;
-        keyword["(*"] = TokenType::LCOMMENT;
-        keyword["*)"] = TokenType::RCOMMENT;
+        
     }
     Scanner(string input){
-        Scanner();
+        keyword = {{"and", "AND"}, {"array", "ARRAY"}, {"asm", "ASM"}, {"begin", "BEGIN"}, {"break", "BREAK"}, {"case", "CASE"}, {"const", "CONST"}, {"constructor", "CONSTRUCTOR"}, {"continue", "CONTINUE"}, {"destructor", "DESTRUCTOR"}, {"div", "DIV"}, {"do", "DO"}, {"downto", "DOWNTO"}, {"else", "ELSE"}, {"end", "END"}, {"FALSE", "FALSE"}, {"file", "FILE"}, {"for", "FOR"}, {"function", "FUNCTION"}, {"goto", "GOTO"}, {"if", "IF"}, {"implementation", "IMPLEMENTATION"}, {"in", "IN"}, {"inline", "INLINE"}, {"interface", "INTERFACE"}, {"label", "LABEL"}, {"mod", "MOD"}, {"nil", "NIL"}, {"not", "NOT"}, {"object", "OBJECT"}, {"of", "OF"}, {"on", "ON"}, {"operator", "OPERATOR"}, {"or", "OR"}, {"packed", "PACKED"}, {"procedure", "PROCEDURE"}, {"program", "PROGRAM"}, {"record", "RECORD"}, {"repeat", "REPEAT"}, {"set", "SET"}, {"shl", "SHL"}, {"SHR", "SHR"}, {"string", "STRING"}, {"then", "THEN"}, {"to", "TO"}, {"true", "TRUE"}, {"type", "TYPE"}, {"unit", "UNIT"}, {"until", "UNTIL"}, {"uses", "USES"}, {"var", "VAR"}, {"while", "WHILE"}, {"with", "WITH"}, {"xor", "XOR"}, {"(integer)", "INTEGER"}, {"(real number)", "REAL"}, {"(identifier)", "INDENTIFIER"}, {"+", "PLUSOP"}, {"-", "MINUSOP"}, {"*", "MULTOP"}, {"/", "DIVOP"}, {":=", "ASSIGN"}, {"=", "EQUAL"}, {"<>", "NE"}, {"<=", "LTEQ"}, {">=", "GTEQ"}, {"<", "LT"}, {">", "GT"}, {"+=", "PLUSEQUAL"}, {"-=]", "MINUSEQUAL"}, {"*=", "MULTEQUAL"}, {"/=", "DIVEQUAL"}, {"^", "CARAT"}, {"}, ", "SEMICOLON"}, {",", "COMMA"}, {"(", "LPAREN"}, {")", "RPAREN"}, {"[", "LBRACKET"}, {"]", "RBRACKET"}, {"{", "LBRACE"}, {"}", "RBRACE"}, {"(*", "LCOMMENT"}, {"*)", "RCOMMENT"}};
+        processdata(input);
     }
     void processdata(string input){
         unsigned long l = input.length();
@@ -214,7 +133,7 @@ class Scanner {
                     in_stream.push_back(keyword[currentSymbol]);
                     currentSymbol = "";
                 }
-                if(currentWord == ""){
+                if(currentWord.find("") == string::npos){
                     currentNumber.push_back(in);
                 }
                 else{
@@ -224,76 +143,105 @@ class Scanner {
             }
             else if((in  == '+') || (in == '-') || (in == '*') || (in == '/') || (in == ':') || (in == '=') || (in == '<') || (in == '>') || (in =='^') || (in == ';') || (in == ',') || (in == '(')
                     || (in == ')') || (in == '[') || (in == ']') || (in == '{') || (in == '}')){//if char is special symbol
-                if(currentWord != ""){
-                    if(keyword.count(currentWord) != 0) {//detect if the key exists in map
+                if(currentState == 0) {
+                    currentSymbol.push_back(in);
+                }
+                else if(currentWord.find("") != string::npos){
+                    if(keyword.find(currentWord) != keyword.end()) {//detect if the key exists in map
                         in_stream.push_back(keyword[currentWord]);//if it exists, store it in vector
                     }
                     else {
-                        in_stream.push_back(TokenType::INDENTIFIER);//not a reserved word, it is identifier
+                        in_stream.push_back("INDENTIFIER");//not a reserved word, it is identifier
                     }
+                    currentSymbol.push_back(in);
                     currentWord = "";//set to empty since we've come accross a symbol token
                 }
                 else if(currentState == 2) {//previous state is a number
                     if(currentNumber.find('.') != std::string::npos){//if current number has a decimal point
-                        in_stream.push_back(TokenType::REAL);
+                        in_stream.push_back("REAL");
                     }
                     else{
-                        in_stream.push_back(TokenType::INTEGER);
+                        in_stream.push_back("INTEGER");
                     }
+                    currentSymbol.push_back(in);
                     currentNumber = "";
+                }
+                else if(currentState == 3){//if prev char is a symbol -> (* or >=
+                    string hypo = currentSymbol;
+                    hypo.push_back(in);
+                    if(keyword.count(hypo) == 0) {
+                        in_stream.push_back(keyword[currentSymbol]);
+                        currentSymbol = "";
+                        currentSymbol.push_back(in);
+                    }
+                    else {
+                        in_stream.push_back(keyword[hypo]);
+                        currentSymbol = "";
+                    }
+                }
+                else if(currentState == 4) {
+                    currentSymbol.push_back(in);
                 }
                 currentState = 3;
                 
             }
             else if((in == ' ') || (in == '\n') || (in == '\t')){//if spaces detected
-                if(currentWord != ""){//if current word NOT empty & cover state 1
+                if(currentWord.find("") != string::npos){//if current word NOT empty & cover state 1
                     in_stream.push_back(keyword[currentWord]);
                     currentWord = "";//reset current word to be empty
                 }
-                else if(currentNumber != ""){//decide whether it is integer or real
+                else if(currentNumber.find("") != string::npos){//decide whether it is integer or real
                     if(currentNumber.find('.') != std::string::npos){//if current number has a decimal point
-                        in_stream.push_back(TokenType::REAL);
+                        in_stream.push_back("REAL");
                     }
                     else{
-                        in_stream.push_back(TokenType::INTEGER);
+                        in_stream.push_back("INTEGER");
                     }
                     currentNumber = "";
                 }
-                else if(currentSymbol != ""){
+                else if(currentSymbol.find("") != string::npos){
                     in_stream.push_back(keyword[currentSymbol]);
                     currentSymbol = "";
                 }
                 currentState = 4;
             }
             else {
-                cout << "Unrecgonized state/token/symbol." << endl;
+                cout << "Unrecgonized state/token/symbol.  " << in << endl;
             }
                 
         }
-        if(currentWord != ""){//if current word NOT empty & cover state 1
+        if(currentWord.find("") != string::npos){//if current word NOT empty & cover state 1
             in_stream.push_back(keyword[currentWord]);
             currentWord = "";//reset current word to be empty
         }
-        else if(currentNumber != ""){//decide whether it is integer or real
+        else if(currentNumber.find("") != string::npos){//decide whether it is integer or real
             if(currentNumber.find('.') != std::string::npos){//if current number has a decimal point
-                in_stream.push_back(TokenType::REAL);
+                in_stream.push_back("REAL");
             }
             else{
-                in_stream.push_back(TokenType::INTEGER);
+                in_stream.push_back("INTEGER");
             }
             currentNumber = "";
         }
-        else if(currentSymbol != ""){
+        else if(currentSymbol.find("") != string::npos){
             in_stream.push_back(keyword[currentSymbol]);
             currentSymbol = "";
         }
-        cout << "EOF" << endl;
+        //cout << "EOF" << endl;
     }
-    TokenType nexttoken();//function to access next char in input file
-    
+    string nexttoken() {//function to access next char in input file
+        if(in_stream.size() > tokenIndex) {
+            string token = in_stream[tokenIndex];
+            tokenIndex++;
+            token.insert(0, to_string(tokenIndex));
+            return token;
+        }
+        return "EOF";
+    }
     private:
-    map<string, TokenType> keyword;
-    vector<TokenType> in_stream; //storing tokens after found from input stream
+    int tokenIndex = 0;
+    map<string, string> keyword;
+    vector<string> in_stream; //storing tokens after found from input stream
 };
 
 int main(int argc, const char * argv[]) {
@@ -308,6 +256,12 @@ int main(int argc, const char * argv[]) {
         while(getline(textfile,str)){
             inputtext+=str;
             inputtext.push_back('\n');
+        }
+        Scanner scanner = Scanner(inputtext);
+        string token = scanner.nexttoken();
+        while(token != "EOF") {
+            cout << token << endl;
+            token = scanner.nexttoken();
         }
         cout << inputtext << endl;    }
     else{
